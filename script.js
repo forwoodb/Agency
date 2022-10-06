dd71491a5b4770970aa720349dde207cba5049b0
Brent Forwood <forwoodb77@gmail.com>
2022-10-06 13:28:40 -0400

    First commit

---
+++ script.js
@@ -0,0 +1,7 @@
+// ********** Navigation **********
+const hamburger = document.querySelector('.hamburger')
+const navMenu = document.querySelector('ul')
+
+hamburger.addEventListener('click', function () {
+    navMenu.classList.toggle('active')
+})
